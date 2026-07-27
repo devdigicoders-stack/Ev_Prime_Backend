ce5d544fc5e02ce61b2f07f50e931038ca73990c
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-07-27 16:07:22 +0530

    done

--- src/routes/rewardRoutes.js
+++ src/routes/rewardRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getRewards, checkDiscount, applyDiscount, addPoints } = require('../controllers/rewardController');
-const { protectUser, protectAdmin } = require('../middlewares/authMiddleware');
+const { protectUser, protect } = require('../middlewares/authMiddleware');
 
 router.get('/', protectUser, getRewards);
 router.get('/discount-check', protectUser, checkDiscount);
 router.post('/apply-discount', protectUser, applyDiscount);
-router.post('/add-points', protectAdmin, addPoints);
+router.post('/add-points', protect, addPoints);
 
 module.exports = router;
